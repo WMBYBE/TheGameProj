65f09a969982be4415a49b8f36cf28de0f32d857
WMBYBE <williambbrouillette@gmail.com>
2024-09-16 01:00:27 -0500

    mian?

--- main.js
+++ main.js
@@ -22,6 +22,24 @@ function hireCrab(){
    };
    var nextCost = Math.floor(10 * Math.pow(1.1,crabs));
    document.getElementById('crabCost').innerHTML = nextCost;
+
+   document.getElementById('pyxelCanvas').style.display = 'block'; };
+async function hireCrab() {
+    const pCanvas = document.getElementById('pyxelCanvas');
+    pyxel.init({
+        canvas: pCanvas,
+        width: 160,
+        height: 120,
+        title: "Crabs"
+    });
+
+    async function LoadGameScript() {
+    const response = await fetch('main.py');
+    return await response.text();
+    
+};
+const gameScript = LoadGameScript();
+    pyxel.run(gameScript);
 };
 function hireLobster(){
     var lobsterCost = Math.floor(100* Math.pow(1.1,lobsters));
